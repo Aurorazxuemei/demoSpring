5468372b393c3371c3adb4f30b249ea56d102753
LAPTOP-FI0OCLVC\zhang <zhangxuemei.19870928@gmail.com>
2025-07-25 16:45:33 +0900

    ログイン成功時、失敗時の処理を実装する

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -8,15 +8,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login")
-    public String View(Model model, LoginForm loginForm){
 
+    private static final String LOGIN_ID = "user";
+    private static final String PASSWORD = "pwd";
+
+    @GetMapping("/login")
+    public String View(Model model, LoginForm loginForm) {
+        model.addAttribute("loginForm", loginForm);
         return "login";
     }
-@PostMapping("/login")
-    public void login(LoginForm loginForm){
-        System.out.println(loginForm.toString());
-}
+
+    @PostMapping("/login")
+    public String login(Model model,LoginForm loginForm) {
+        var isCorrectUserAuth = loginForm.getLoginId().equals(LOGIN_ID)
+                && loginForm.getPassword().equals(PASSWORD);
+        if (isCorrectUserAuth) {
+            return "redirect:/menu";
+        }else{
+            model.addAttribute("errorMsg", "ログインIDとパスワードの組み合わせが間違っています");
+            return "login";
+        }
+    }
 
 
 }
