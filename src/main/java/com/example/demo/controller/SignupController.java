c9447f68a1a65023de068d014fa87bf88ae00dca
LAPTOP-FI0OCLVC\zhang <zhangxuemei.19870928@gmail.com>
2025-07-31 11:55:47 +0900

    アノテーションを使って超簡単に入力チェックを実装します

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -14,6 +14,8 @@ import org.springframework.context.MessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -33,13 +35,33 @@ public class SignupController {
         return "signup";
     }
 
+    /**
+     * ユーザー登録
+     * @param model　モデル
+     * @param signupForm　フォームの入力情報
+     * @param bindingResult　入力チェック結果
+     */
     @PostMapping("/signup")
-    public void login(Model model, SignupForm signupForm) {
+    public void login(Model model, @Validated SignupForm signupForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+//            String message = AppUtil.getMessage(messageSource,MessageConst.FORM_ERROR);
+//            model.addAttribute("message", message);
+//            model.addAttribute("isError",true);
+            editGuideMessage(model,MessageConst.FORM_ERROR,true);
+            return;
+        }
         var userInfo = signupService.registerUserInfo(signupForm);
         var signupMessage =judgeMessageKey(userInfo);
-        String message = AppUtil.getMessage(messageSource, signupMessage.getMessageId());
+//        String message = AppUtil.getMessage(messageSource, signupMessage.getMessageId());
+//        model.addAttribute("message", message);
+//        model.addAttribute("isError", signupMessage.isError());
+        editGuideMessage(model,signupMessage.getMessageId(),signupMessage.isError());
+    }
+
+    private void editGuideMessage(Model model,String messageId,Boolean isError) {
+        String message = AppUtil.getMessage(messageSource,messageId);
         model.addAttribute("message", message);
-        model.addAttribute("isError", signupMessage.isError());
+        model.addAttribute("isError", isError);
     }
 
     private SinupMessage judgeMessageKey(Optional<UserInfo> userInfo) {
--- src/main/java/com/example/demo/form/SignupForm.java
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -1,9 +1,14 @@
 package com.example.demo.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class SignupForm {
+    /**ログインID*/
+    @Length(min=8,max=20)
     private String loginId;
+    /**パスワード*/
+    @Length(min=8,max=20)
     private String password;
 }
